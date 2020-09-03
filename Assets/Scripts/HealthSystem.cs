2537254ee94fd7516e371f2bc30156c2e50e12a4
OblivionLS <55786167+OblivionLS@users.noreply.github.com>
2020-09-03 14:41:16 +0200

    Score on End-Screen

--- Assets/Scripts/HealthSystem.cs
+++ Assets/Scripts/HealthSystem.cs
@@ -6,7 +6,7 @@ using UnityEngine.SceneManagement;
 public class HealthSystem : MonoBehaviour
 {
     public int startHealth = 3;
-    public int currentHealth;
+    public IntVariable currentHealth;
     [Tooltip("low to high - highest gets disabled first")]public GameObject[] healthPartsVisuals;
 
     public void Start()
@@ -19,18 +19,18 @@ public class HealthSystem : MonoBehaviour
     /// <returns></returns>
     public int TakeDamage()
     {
-        currentHealth -= 1;
+        currentHealth.ApplyChange(-1);
         //deactivates highest active object of the List
-        if(currentHealth < healthPartsVisuals.Length && currentHealth != 0)
+        if(currentHealth.value < healthPartsVisuals.Length && currentHealth.value != 0)
         {
-            healthPartsVisuals[currentHealth].SetActive(false);
+            healthPartsVisuals[currentHealth.value].SetActive(false);
         }
         //change to end scene if health is 0
-        if(currentHealth == 0)
+        if(currentHealth.value == 0)
         {
             SceneManager.LoadScene("EndScene");
         }
-        return currentHealth;
+        return currentHealth.value;
     }
 
     /// <summary>
@@ -38,7 +38,7 @@ public class HealthSystem : MonoBehaviour
     /// </summary>
     public void ResetHealth()
     {
-        currentHealth = startHealth;
+        currentHealth.SetValue(startHealth);
         foreach (GameObject visuals in healthPartsVisuals)
         {
             visuals.SetActive(true);
---
+++ Assets/Scripts/ScoreDisplayEnd.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreDisplayEnd : MonoBehaviour
+{
+    public GameObject scoreText;
+    public IntVariable score;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        scoreText.GetComponent<Text>().text = "Score: " + score.value;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
