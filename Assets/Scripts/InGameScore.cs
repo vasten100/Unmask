ac41d8d150498d6bc48b1b62dd7d4935bb591d25
OblivionLS <55786167+OblivionLS@users.noreply.github.com>
2020-09-05 16:10:51 +0200

    In game score

---
+++ Assets/Scripts/InGameScore.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InGameScore : MonoBehaviour
+{
+
+    public GameObject text;
+
+    public IntVariable Score;
+    // Start is called before the first frame update
+    void Start()
+    {
+        text.GetComponent<Text>().text = "0";
+    }
+
+
+
+    public void UpdateScore()
+    {
+        text.GetComponent<Text>().text = Score.value.ToString();
+    }
+}
